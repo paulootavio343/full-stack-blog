cf831c36801da3558894a34c2ade7864662294f0
Paulo Otávio <92699268+paulootavio343@users.noreply.github.com>
2022-12-06 01:57:32 -0300

    Use PostgreSQL instead of SQLite

--- blog/settings.py
+++ blog/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME_DB'),
+        'USER': config('USER_DB'),
+        'PASSWORD': config('PASSWORD_DB'),
+        'HOST': config('HOST_DB'),
+        'PORT': config('PORT_DB'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
